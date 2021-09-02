346667cd1d2d066166127e91c222b22183eb81eb
somm12 <thals8744@naver.com>
2021-09-02 10:51:37 +0900

    component life cycle

--- src/App.js
+++ src/App.js
@@ -1,73 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://www.wearesovegan.com/wp-content/uploads/2020/09/howtomakevegankimchirecipe-h3.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Ramen",
-    image:
-      "https://www.wearesovegan.com/wp-content/uploads/2020/09/howtomakevegankimchirecipe-h3.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "Bulgogi",
-    image:
-      "https://www.wearesovegan.com/wp-content/uploads/2020/09/howtomakevegankimchirecipe-h3.jpg",
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: "teokbokgi",
-    image:
-      "https://www.wearesovegan.com/wp-content/uploads/2020/09/howtomakevegankimchirecipe-h3.jpg",
-    rating: 4.5,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "https://www.wearesovegan.com/wp-content/uploads/2020/09/howtomakevegankimchirecipe-h3.jpg",
-    rating: 4.4,
-  },
-];
-//prop-types is for checking if sending props are same with receiving props
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating} / 5.0 </h4>
-      <img src={picture} alt="" />
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  //state => the place that data is put. and the data is changed
+  state = {
+    count: 0,
+  };
+  //everytime you call setstate, react will rerender
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("component updated");
+  }
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
